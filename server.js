1e14712e4921a6f8d3ddac70d6b7b14cfbebc9b4
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-07-17 23:21:46 +0200

    Init project

--- server.js
+++ server.js
@@ -1,34 +1,62 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
 var app = express();
+var googleProfile = {};
+
+passport.serializeUser(function(user, done) { //??
+    done(null, user);
+});
+passport.deserializeUser(function(obj, done) { //??
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret:config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+      id: profile.id,
+      displayName: profile.displayName
+    };
+    cb(null, profile); //??
+  }
+));
 
 app.set('view engine', 'pug');
 app.set('views','./views');
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/store', function(req, res, next){
-  console.log('jestes w middleware');
-  next();
-});
+// app.use('/store', function(req, res, next){
+//   console.log('jestes w middleware');
+//   next();
+// });
 
+// app routes
 app.get('/', function(req, res) {
-  res.send('Welcome to our application');
+  res.render('index', {user: req.user});
 });
 
-app.get('/dynamic-app', function(req, res){
-  res.render('dynamic-app', {
-    name: "Sign In",
-    LogIn: "http://www.google.com",
-    LogIn2: "/auth/google"
-  });
+app.get('/logged', function(req, res) {
+  res.render('logged', {user: googleProfile});
 });
 
-app.get('/auth/google', function(req, res) {
-  res.render('auth/google', {
-    name: "Pawel",
-    email: "https://www.google.com/intl/pl/gmail/about/"
-  });
-});
+app.get('/auth/google', passport.authenticate('google', {
+  scope : ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect : '/logged',
+  failureRedirect: '/'
+  })
+);
 
 app.listen(3000);
-app.use(function(req, res, next){
-  res.status(400).send('error 404.Check your adress');
-});
+// app.use(function(req, res, next){
+//   res.status(400).send('error 404.Check your adress');
+// });
